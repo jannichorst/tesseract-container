9b100bfda362a772f1fb6547a1632dfc6ff5c94c
Jannic Horst <jan-n.ht@gmx.de>
2024-06-01 17:27:57 +0200

    linting, formatting and adding dev requirements

--- src/app/database.py
+++ src/app/database.py
@@ -1,14 +1,17 @@
 import sqlite3
 import uuid
 from datetime import datetime
+
 from fastapi import HTTPException
+
 
 def init_db(database_url, logger):
     conn = sqlite3.connect(database_url)
     cursor = conn.cursor()
-    
+
     # Create jobs table
-    cursor.execute('''
+    cursor.execute(
+        """
     CREATE TABLE IF NOT EXISTS jobs (
         uuid TEXT PRIMARY KEY,
         file_name TEXT,
@@ -18,10 +21,12 @@ def init_db(database_url, logger):
         width INTEGER,
         height INTEGER
     )
-    ''')
-    
+    """
+    )
+
     # Create ocr_results table with foreign key to jobs table
-    cursor.execute('''
+    cursor.execute(
+        """
     CREATE TABLE IF NOT EXISTS ocr_results (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         uuid TEXT,
@@ -39,12 +44,14 @@ def init_db(database_url, logger):
         text TEXT,
         FOREIGN KEY (uuid) REFERENCES jobs(uuid)
     )
-    ''')
-    
+    """
+    )
+
     conn.commit()
     conn.close()
     logger.info(f"Database initialized at {database_url}")
     print(f"Database initialized at {database_url}")
+
 
 def check_db_operations(DB_PATH):
     try:
@@ -53,23 +60,28 @@ def check_db_operations(DB_PATH):
 
         # Insert a test entry
         test_uuid = str(uuid.uuid4())
-        cursor.execute('INSERT INTO jobs (uuid, file_name, start_datetime, status) VALUES (?, ?, ?, ?)',
-                       (test_uuid, 'test_file', datetime.now().isoformat(), 'test_status'))
+        cursor.execute(
+            """INSERT INTO jobs (uuid, file_name, start_datetime,
+                       status) VALUES (?, ?, ?, ?)""",
+            (test_uuid, "test_file", datetime.now().isoformat(), "test_status"),
+        )
 
         # Verify insertion
-        cursor.execute('SELECT * FROM jobs WHERE uuid = ?', (test_uuid,))
+        cursor.execute("SELECT * FROM jobs WHERE uuid = ?", (test_uuid,))
         if cursor.fetchone() is None:
             raise Exception("Test insertion failed")
 
         # Delete the test entry
-        cursor.execute('DELETE FROM jobs WHERE uuid = ?', (test_uuid,))
+        cursor.execute("DELETE FROM jobs WHERE uuid = ?", (test_uuid,))
 
         # Verify deletion
-        cursor.execute('SELECT * FROM jobs WHERE uuid = ?', (test_uuid,))
+        cursor.execute("SELECT * FROM jobs WHERE uuid = ?", (test_uuid,))
         if cursor.fetchone() is not None:
             raise Exception("Test deletion failed")
 
         conn.commit()
         conn.close()
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Database operation check failed: {e}")
+        raise HTTPException(
+            status_code=500, detail=f"Database operation check failed: {e}"
+        )
--- src/app/main.py
+++ src/app/main.py
@@ -1,18 +1,20 @@
-import logging
-from fastapi import FastAPI, File, UploadFile, BackgroundTasks, HTTPException
-from fastapi.responses import JSONResponse
-import pytesseract
-import sqlite3
 import asyncio
-import uuid
-from datetime import datetime
-from contextlib import asynccontextmanager
-import subprocess
-import shutil
+import logging
 import os
-from PIL import Image
+import shutil
+import sqlite3
+import subprocess
+import uuid
+from contextlib import asynccontextmanager
+from datetime import datetime
 from io import BytesIO
-from app.database import init_db, check_db_operations
+
+import pytesseract
+from fastapi import BackgroundTasks, FastAPI, File, HTTPException, UploadFile
+from fastapi.responses import JSONResponse
+from PIL import Image
+
+from app.database import check_db_operations, init_db
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -20,9 +22,10 @@ logger = logging.getLogger("uvicorn")
 logger.propagate = False
 
 # Variables
-DB_PATH = 'app/ocr_results.db'
+DB_PATH = "app/ocr_results.db"
 PORT = 8000
-HOST = '0.0.0.0'
+HOST = "0.0.0.0"
+
 
 def list_directory_structure(start_path="."):
     directory_structure = {}
@@ -35,42 +38,59 @@ def list_directory_structure(start_path="."):
         subdir["files"] = files
     return directory_structure
 
+
 async def process_image(contents, task_id):
     await asyncio.sleep(0)  # Yield control to the event loop
     pil_image = Image.open(BytesIO(contents))
-        
+
     width, height = pil_image.size
-    
+
     # Ensure the image is in RGB mode
     pil_image = pil_image.convert("RGB")
-    
+
     data = pytesseract.image_to_data(pil_image, output_type=pytesseract.Output.DICT)
-    
+
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    
-    for i in range(len(data['level'])):
-        cursor.execute('''
+
+    for i in range(len(data["level"])):
+        cursor.execute(
+            """
         INSERT INTO ocr_results (
             uuid, level, page_num, block_num, par_num,
             line_num, word_num, left, top, width, height,
             conf, text
         ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        ''', (
-            task_id, data['level'][i], data['page_num'][i], data['block_num'][i],
-            data['par_num'][i], data['line_num'][i], data['word_num'][i],
-            data['left'][i], data['top'][i], data['width'][i], data['height'][i],
-            data['conf'][i], data['text'][i]
-        ))
-    
-    cursor.execute('''
+        """,
+            (
+                task_id,
+                data["level"][i],
+                data["page_num"][i],
+                data["block_num"][i],
+                data["par_num"][i],
+                data["line_num"][i],
+                data["word_num"][i],
+                data["left"][i],
+                data["top"][i],
+                data["width"][i],
+                data["height"][i],
+                data["conf"][i],
+                data["text"][i],
+            ),
+        )
+
+    cursor.execute(
+        """
     UPDATE jobs
     SET end_datetime = ?, status = ?, width = ?, height = ?
     WHERE uuid = ?
-    ''', (datetime.now().isoformat(), 'completed', width, height, task_id))
-    
+    """,
+        (datetime.now().isoformat(), "completed", width, height, task_id),
+    )
+
     conn.commit()
     conn.close()
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -78,7 +98,9 @@ async def lifespan(app: FastAPI):
     init_db(DB_PATH, logger)
     yield
 
+
 app = FastAPI(lifespan=lifespan)
+
 
 @app.post("/analyzeDocument/")
 async def upload_image(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
@@ -87,55 +109,70 @@ async def upload_image(background_tasks: BackgroundTasks, file: UploadFile = Fil
     contents = await file.read()
     task_id = str(uuid.uuid4())  # Generate a new UUID for each task
     start_datetime = datetime.now().isoformat()
-    
+
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO jobs (uuid, file_name, start_datetime, status) VALUES (?, ?, ?, ?)',
-                   (task_id, file.filename, start_datetime, 'in_progress'))
+    cursor.execute(
+        """INSERT INTO jobs (uuid, file_name, start_datetime,
+                   status) VALUES (?, ?, ?, ?)""",
+        (task_id, file.filename, start_datetime, "in_progress"),
+    )
     conn.commit()
     conn.close()
-    
+
     background_tasks.add_task(process_image, contents, task_id)
     return JSONResponse(content={"task_id": task_id})
+
 
 @app.get("/analyzeResults/{task_id}")
 async def get_result(task_id: str):
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
-    
+
     # Get job info
-    cursor.execute('SELECT file_name, start_datetime, end_datetime, status, width, height FROM jobs WHERE uuid = ?', (task_id,))
+    cursor.execute(
+        """ SELECT file_name, start_datetime, end_datetime, status,
+                    width, height FROM jobs WHERE uuid = ? """,
+        (task_id,),
+    )
     job_row = cursor.fetchone()
-    
+
     if not job_row:
         conn.close()
         raise HTTPException(status_code=404, detail="Task not found")
-    
+
     file_name, start_datetime, end_datetime, status, width, height = job_row
-    
+
     # Get OCR results
-    cursor.execute('SELECT level, page_num, block_num, par_num, line_num, word_num, left, top, width, height, conf, text FROM ocr_results WHERE uuid = ?', (task_id,))
+    cursor.execute(
+        """ SELECT level, page_num, block_num, par_num, line_num,
+                    word_num, left, top, width, height, conf,
+                    text FROM ocr_results WHERE uuid = ? """,
+        (task_id,),
+    )
     ocr_rows = cursor.fetchall()
     conn.close()
-    
-    if status == 'completed':
+
+    if status == "completed":
         results = []
         for row in ocr_rows:
-            results.append({
-                'level': row[0],
-                'page_num': row[1],
-                'block_num': row[2],
-                'par_num': row[3],
-                'line_num': row[4],
-                'word_num': row[5],
-                'left': row[6],
-                'top': row[7],
-                'width': row[8],
-                'height': row[9],
-                'conf': row[10],
-                'text': row[11],
-            })
-        
+            results.append(
+                {
+                    "level": row[0],
+                    "page_num": row[1],
+                    "block_num": row[2],
+                    "par_num": row[3],
+                    "line_num": row[4],
+                    "word_num": row[5],
+                    "left": row[6],
+                    "top": row[7],
+                    "width": row[8],
+                    "height": row[9],
+                    "conf": row[10],
+                    "text": row[11],
+                }
+            )
+
         response = {
             "state": "SUCCESS",
             "file_name": file_name,
@@ -144,7 +181,7 @@ async def get_result(task_id: str):
             "status": status,
             "width": width,
             "height": height,
-            "results": results
+            "results": results,
         }
     else:
         response = {
@@ -154,16 +191,19 @@ async def get_result(task_id: str):
             "start_datetime": start_datetime,
             "end_datetime": end_datetime,
             "width": width,
-            "height": height
+            "height": height,
         }
-    
+
     return JSONResponse(content=response)
+
 
 @app.get("/health")
 async def health_check():
     try:
         # Check Tesseract availability
-        result = subprocess.run(['tesseract', '--version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run(
+            ["tesseract", "--version"], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
         if result.returncode != 0:
             raise HTTPException(status_code=500, detail="Tesseract is not available")
 
@@ -171,35 +211,41 @@ async def health_check():
         total, used, free = shutil.disk_usage("/")
         if free < 1 * 1024 * 1024 * 1024:  # Less than 1GB free space
             raise HTTPException(status_code=500, detail="Not enough disk space")
-        
+
         # Check if db file exists
         if not os.path.exists(DB_PATH):
             for root, dirs, files in os.walk("."):
                 if "ocr_results.db" in files:
                     found_path = os.path.join(root, "ocr_results.db")
-                    raise HTTPException(status_code=500, detail=f"Health check failed: Database file found at {found_path}, but not at the expected location.")
+                    raise HTTPException(
+                        status_code=500,
+                        detail=f"""Health check failed:
+                                         Database file found at {found_path},
+                                         but not at the expected location. """,
+                    )
             else:
                 raise FileNotFoundError("Database file not found")
 
-                # Check database connection
-        
         # Easy DB connection check
         try:
             conn = sqlite3.connect(DB_PATH)
             cursor = conn.cursor()
-            cursor.execute('SELECT 1')
+            cursor.execute("SELECT 1")
             conn.commit()
             conn.close()
         except Exception as e:
-            raise HTTPException(status_code=500, detail="Could not connect to database.")
+            raise HTTPException(
+                status_code=500, detail=f"Could not connect to database: {e}"
+            )
 
         # Check database connection and operations
         check_db_operations(DB_PATH)
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Health check failed: {e}")
-    
+
     return JSONResponse(content={"status": "healthy"})
+
 
 @app.get("/fs")
 async def file_system_structure():
@@ -207,8 +253,14 @@ async def file_system_structure():
         structure = list_directory_structure()
         return JSONResponse(content=structure)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Failed to list file system structure: {e}")
+        raise HTTPException(
+            status_code=500,
+            detail=f"""Failed to list file
+                             system structure: {e}""",
+        )
+
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host=HOST, port=PORT)
